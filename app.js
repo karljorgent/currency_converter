6934179b6347e18b9ea471137340dda609783cc0
AxelMartin69 <75273577+AxelMartin69@users.noreply.github.com>
2022-04-08 13:58:32 +0300

    lol it not workin'

---
+++ app.js
@@ -0,0 +1,22 @@
+import $ from "jquery";
+
+// set endpoint and your API key
+endpoint = 'convert';
+access_key = '7d5234ca8df53e5aadd76314344fcb47';
+
+// define from currency, to currency, and amount
+from = 'EUR';
+to = 'GBP';
+amount = '10';
+
+// execute the conversion using the "convert" endpoint:
+$.ajax({
+    url: 'https://data.fixer.io/api/' + endpoint + '?access_key=' + access_key +'&from=' + from + '&to=' + to + '&amount=' + amount,   
+    dataType: 'jsonp',
+    success: function(json) {
+
+        // access the conversion result in json.result
+        alert(json.result);
+                
+    }
+});
